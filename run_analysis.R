4f6741bfe76c0c0b651a48bb46457c1eccff889a
tedswalk <nadine_chew@hotmail.com>
2014-10-20 20:09:44 +1100

    Update run_analysis.R
    My final piece of code for the project.

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 
 # x_test/x_train are data set of the 561 feature vectors
 # y_test/y_train are labels of the 6 feature activities
-# sub_test/sub_train are volunteers ID for the activities recorded
+# sub_test/sub_train are the 30 volunteers for the activities recorded
 
 ## Read Test data set into R
 x_test <- read.table("./test/X_test.txt")
@@ -14,7 +14,7 @@ x_train <- read.table("./train/X_train.txt")
 y_train <- read.table("./train/y_train.txt")
 sub_train <- read.table("./train/subject_train.txt")
 
-## Merge the files into 3 data sets for now
+## Merge the files into 3 data sets
 y_combine <- rbind(y_train,y_test)
 x_combine <- rbind(x_train,x_test)
 subject_combine <- rbind(sub_train,sub_test)
@@ -35,7 +35,7 @@ newlist2 <- newlist[!newlist %in% meanFreq]
 x_new <- x_combine[,newlist2]
 
 ## Assign variable names to subject_combine and y_combine data set
-colnames(subject_combine) <- "subject_id"
+colnames(subject_combine) <- "subject"
 colnames(y_combine) <- "activity"
 
 ## Merge all data into one big data set
@@ -66,13 +66,32 @@ for (i in 1:10299){
   }
 }
 
-## Labels the data set with descriptive variable names
-#sub("t","Time",names(combine_list))
-#sub("f","Frequency",names(combine_list))
-names(combine_list) <- gsub("Acc","Acceleration",names(combine_list))
-names(combine_list) <- gsub("X","X-axial",names(combine_list))
-names(combine_list) <- gsub("Y","Y-axial",names(combine_list))
-names(combine_list) <- gsub("Z","Z-axial",names(combine_list))
-names(combine_list) <- gsub("Mag","Magnitude",names(combine_list))
-names(combine_list) <- gsub("Gyro","AngularVelocity",names(combine_list))
-names(combine_list) <- gsub("std","StandardDeviation",names(combine_list))
+# change all to lower case and assign descriptive names to each variable
+names(combine_list) <- tolower(names(combine_list))
+names(combine_list) <- gsub("-"," ",names(combine_list))
+names(combine_list) <- sub("mean..","mean",names(combine_list))
+names(combine_list) <- sub("std..","standard deviation",names(combine_list))
+names(combine_list) <- sub("tbodyaccjerkmag","time body acceleration jerk magnification",names(combine_list))
+names(combine_list) <- sub("tbodyaccjerk","time body acceleration jerk",names(combine_list))
+names(combine_list) <- sub("tbodygyrojerkmag","time body gyroscope jerk magnification",names(combine_list))
+names(combine_list) <- sub("tbodygyrojerk","time body gyroscope jerk",names(combine_list))
+names(combine_list) <- sub("tgravityaccmag","time gravity acceleration magnification",names(combine_list))
+names(combine_list) <- sub("tbodyaccmag","time body acceleration magnification",names(combine_list))
+names(combine_list) <- sub("tbodyacc","time body acceleration",names(combine_list))
+names(combine_list) <- sub("tgravityacc","time gravity acceleration",names(combine_list))
+names(combine_list) <- sub("tbodygyromag","time body gyroscope magnification",names(combine_list))
+names(combine_list) <- sub("tbodygyro","time body gyroscope",names(combine_list))
+names(combine_list) <- sub("fbodyaccjerk","frequency body acceleration jerk",names(combine_list))
+names(combine_list) <- sub("fbodygyrojerk","frequency body gyroscope jerk",names(combine_list))
+names(combine_list) <- sub("fbodyaccmag","frequency body acceleration mag",names(combine_list))
+names(combine_list) <- sub("fbodyacc","frequency body acceleration",names(combine_list))
+names(combine_list) <- sub("fbodybodygyrojerkmag","frequency body body gyroscope jerk magnification",names(combine_list))
+names(combine_list) <- sub("fbodybodyaccjerkmag","frequency body body acceleration jerk magnification",names(combine_list))
+names(combine_list) <- sub("fbodygyro","frequency body gyroscope",names(combine_list))
+names(combine_list) <- sub("fbodybodygyromag","frequency body body gyroscope magnfication",names(combine_list))
+
+## generate a tidy data with the average of each variable for each activity and each subject
+finalfile <- aggregate(.~ subject + activity,data=combine_list,mean)
+
+## write tidy data into a txt file format
+write.table(finalfile, file="my_tidy_data.txt",row.name=FALSE)
